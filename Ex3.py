e0c651d24b5ef0807721b189242609a2c1ab1cd4
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-07-09 21:45:20 -0300

    feat: testando funções

---
+++ Ex3.py
@@ -0,0 +1,8 @@
+a = round(float(input('digite o primeiro valor: ')),2)
+b = round(float(input('digite o segundo valor: ')))
+
+def soma(a, b):
+        return a + b
+
+resultado = soma(a,b)
+print(resultado)
