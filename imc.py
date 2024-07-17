96d101adcd04cc2b19bfa2555f0c679c7e098764
estrela801 <rohenri0808@gmail.com>
2024-07-16 23:27:42 -0300

    feat: adicionando lista TO-DO e iniciando-a

---
+++ imc.py
@@ -0,0 +1,7 @@
+#todo list
+# imprimir o que o programa fará
+# pedir o peso e a altura do usuario 
+# passar para uma função que verifica e faz os calculos
+# retornar o resultado 
+# imprimir o resultado 
+print('Diga-me os valores de peso e altura para saber sua escala noimc')
